d936d97f15048237c87cde3c42909006f179f9bb
Trent Doiron <103758611+tdoiron0@users.noreply.github.com>
2024-12-28 23:50:05 -0500

    keyboard input testing

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -15,9 +15,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class MainWindow implements ActionListener {
-    public static final int WIDTH = 600;
-    public static final int HEIGHT = 500;
+public class MainWindow {
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 200;
 
     public MainWindow() {
         JFrame frame = new JFrame();
@@ -25,6 +25,7 @@ public class MainWindow implements ActionListener {
         frame.setLayout(new BorderLayout());
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setFocusable(true);
 
         JLabel label = new JLabel("Press the buttons to resize the window.", SwingConstants.CENTER);
         frame.add(label, BorderLayout.CENTER);
@@ -33,13 +34,7 @@ public class MainWindow implements ActionListener {
         buttonPanel.setLayout(new FlowLayout());
 
         JButton increaseButton = new JButton("Increase Size");
-        JButton decreaseButton = new JButton("Decrease Size");
-
-        buttonPanel.add(increaseButton);
-        buttonPanel.add(decreaseButton);
-
-        frame.add(buttonPanel);
-
+        increaseButton.setFocusable(false);
         increaseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -47,6 +42,8 @@ public class MainWindow implements ActionListener {
             }
         });
 
+        JButton decreaseButton = new JButton("Decrease Size");
+        decreaseButton.setFocusable(false);
         decreaseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -54,23 +51,15 @@ public class MainWindow implements ActionListener {
             }
         });
 
-        frame.addKeyListener(new KeyListener() {
-            public void keyTyped(KeyEvent e) {}
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyChar() == ' ') {
-                    frame.setSize(frame.getWidth() + 10, frame.getHeight() + 10);
-                }
-            }
-            public void keyReleased(KeyEvent e) {}
-        });
+        buttonPanel.add(increaseButton);
+        buttonPanel.add(decreaseButton);
+
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        
+
+        frame.addKeyListener(new TestKeyboardListener());
 
         frame.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if ("Button1".equals(e.getActionCommand())) {
-            System.out.println("Hellow World!");
-        }
     }
 }
--- src/TestKeyboardListener.java
+++ src/TestKeyboardListener.java
@@ -6,21 +6,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class CalculatorKeyboardListener implements KeyListener {
+public class TestKeyboardListener implements KeyListener {
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+        System.out.println("Key typed: " + e.getKeyChar());
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyChar() == ' ') {
-            System.out.println("Pressed");
-        }
+        System.out.println("Key pressed: " + e.getKeyChar());
     }
     
     @Override
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyChar() == ' ') {
-            System.out.println("Released");
-        }
+        System.out.println("Key released: " + e.getKeyChar());
     }
 }
